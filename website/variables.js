78c62f4b36ffb23bf2b1f8e1ec1f64a6f9cd003d
Berret Terry <berretterry@ibm.com>
2023-06-08 12:31:30 -0700

    iniitial site build

---
+++ website/variables.js
@@ -0,0 +1,8 @@
+const Info = {
+    repo: 'https://github.com/ibm-client-engineering/solution-mq-gitops.git',
+    project: 'solution-mq-gitops',
+    org: 'ibm-client-engineering',
+    url: 'https://ibm-client-engineering.github.io',
+    baseUrl: '/solution-mq-gitops/'
+};
+module.exports = { Info };
